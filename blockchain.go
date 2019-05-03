1e40c1ea375c75d4699fbffa1bccba0ffb869e7b
xufeng <xufengyongyou@163.com>
2019-05-03 21:10:19 +0800

    修改AddBlockToChain函数

--- blockchain.go
+++ blockchain.go
@@ -20,10 +20,10 @@ func StartBlockChain() (bl *Blockchain){
 
 func (bl *Blockchain) Runing(){
 	fmt.Printf("blockchain running\n")
-	block:=bl.blocks[len(bl.blocks)-1]
+	//block:=bl.blocks[len(bl.blocks)-1]
 	//fmt.Printf("timestamp=%d\n",block.timestamp)
-	b:=CreatBlock("number two block",block.bhead.hash)
-	bl.AddBlockToChain(b)
+	//b:=CreatBlock("number two block",block.bhead.hash)
+	bl.AddBlockToChain("number two block")
 	for idx,value:=range bl.blocks{
 		fmt.Printf("%d,block,hash=%x\n",idx,value.bhead.hash)
 	}
@@ -33,7 +33,8 @@ func GenesisBlock() *Block{
 	Genesisblock:=CreatBlock(message,[]byte{})  //没有prevhash 第一个块，当然没有前向块的哈希
 	return Genesisblock
 }
-func(bl *Blockchain)AddBlockToChain(b *Block){
+func(bl *Blockchain)AddBlockToChain( message string){
 	
-	bl.blocks=append(bl.blocks,b)
+	block:=CreatBlock(message,bl.blocks[len(bl.blocks)-1].bhead.hash)
+	bl.blocks=append(bl.blocks,block)
 }
