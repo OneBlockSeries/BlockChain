919138ddb28951ba08305b744e4e3c3470484924
xufeng <xufengyongyou@163.com>
2019-05-03 21:04:16 +0800

    增加AddBlockToChain

--- blockchain.go
+++ blockchain.go
@@ -20,6 +20,10 @@ func StartBlockChain() (bl *Blockchain){
 
 func (bl *Blockchain) Runing(){
 	fmt.Printf("blockchain running\n")
+	block:=bl.blocks[len(bl.blocks)-1]
+	//fmt.Printf("timestamp=%d\n",block.timestamp)
+	b:=CreatBlock("number two block",block.bhead.hash)
+	bl.AddBlockToChain(b)
 	for idx,value:=range bl.blocks{
 		fmt.Printf("%d,block,hash=%x\n",idx,value.bhead.hash)
 	}
@@ -28,4 +32,8 @@ func GenesisBlock() *Block{
 	message:="Genesis block"
 	Genesisblock:=CreatBlock(message,[]byte{})  //没有prevhash 第一个块，当然没有前向块的哈希
 	return Genesisblock
+}
+func(bl *Blockchain)AddBlockToChain(b *Block){
+	
+	bl.blocks=append(bl.blocks,b)
 }
