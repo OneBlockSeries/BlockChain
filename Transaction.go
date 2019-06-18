72b9bedf9d7d222384ae2dd643ef1db768256d87
xufeng <xufengyongyou@163.com>
2019-06-19 00:02:54 +0800

    增加了Transaction Serialize序列化功能，完善merkletree 功能，

--- Transaction.go
+++ Transaction.go
@@ -62,3 +62,14 @@ func Coinbase(to string) *Transaction {
 	tran.SetId()
 	return &tran
 }
+func (tx Transaction) Serialize() []byte {
+	var encoded bytes.Buffer
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return encoded.Bytes()
+}
